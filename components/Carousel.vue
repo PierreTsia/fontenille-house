8c776bc8c3fcb77230c4748aa4136b4c2816c854
Pierre_T <pierre.tsiakkaros@gmail.com>
2019-05-08 16:37:06 +0200

    :bug: :ambulance: vuetify image component

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <el-carousel indicator-position="outside" :interval="5000">
     <el-carousel-item
       v-for="(image, index) in images"
@@ -7,6 +7,22 @@
     >
       <!--<el-image :src="image.url" :alt="image.title" style="height:100%;">
       </el-image>-->
+
+      <v-img
+        :src="image.url"
+        :lazy-src="image.url"
+        aspect-ratio="1"
+        class="grey lighten-2"
+      >
+        <template v-slot:placeholder>
+          <v-layout fill-height align-center justify-center ma-0>
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-layout>
+        </template>
+      </v-img>
       <span class="imageDescription caption">{{ image.description }}</span>
     </el-carousel-item>
   </el-carousel>
@@ -39,10 +55,8 @@ export default {
   color white
 @media screen and (min-width: 1400px)
   .el-carousel__container
-    border 1px solid red
     height 400px
 @media screen and (min-width: 1600px)
   .el-carousel__container
-    border 1px solid red
     height 600px
 </style>
